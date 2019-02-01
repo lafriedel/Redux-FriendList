2b79de791e49c38f2dea9fb78550b2dd27189504
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-31 19:08:28 -0500

    Refactor to store input state in App

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,40 @@ import React, { Component } from "react";
 import FriendsListContainer from "./containers/FriendsListContainer";
 import FriendFormContainer from "./containers/FriendFormContainer";
 
-import { getFriends } from "./actions";
+import { getFriends, postNewFriend, updateFriend } from "./actions";
 import { connect } from "react-redux";
 
 import "./App.css";
 
 class App extends Component {
+  state = {
+    friend: {
+      name: "",
+      age: "",
+      email: ""
+    }
+  };
+
+  handleInputChanges = e => {
+      this.setState({
+        friend: {
+          ...this.state.friend,
+          [e.target.name]: e.target.value
+        }
+
+      })
+  };
+
+  addFriend = (e) => {
+    e.preventDefault();
+      this.props.postNewFriend(this.state.friend);
+  };
+
+  updateFriend = e => {
+    e.preventDefault();
+    this.props.updateFriend(this.state.friend);
+  }
+
   componentDidMount() {
     this.props.getFriends();
   }
@@ -19,14 +47,25 @@ class App extends Component {
           <h1>Friends or Frenemies?</h1>
         </div>
 
-        <FriendFormContainer />
+        <FriendFormContainer 
+          handleInputChanges={this.handleInputChanges}
+          addFriend={this.addFriend}
+          updateFriend={this.updateFriend}
+          friend={this.state.friend}
+          isUpdating={this.props.isUpdating}
+        />
         <FriendsListContainer />
       </>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  friends: state.friends,
+  isUpdating: state.isUpdating
+})
+
 export default connect(
-  null,
-  { getFriends }
+  mapStateToProps,
+  { getFriends, postNewFriend, updateFriend }
 )(App);
--- friends/src/containers/FriendFormContainer.js
+++ friends/src/containers/FriendFormContainer.js
@@ -1,47 +1,47 @@
 import React from "react";
-import { connect } from "react-redux";
-import { postNewFriend, updateFriend } from "../actions";
+// import { connect } from "react-redux";
+// import { postNewFriend, updateFriend } from "../actions";
 
 import FriendForm from "../components/Friends/FriendForm";
 
 class FriendFormContainer extends React.Component {
-  state = {
-    friend: {
-      name: "",
-      age: "",
-      email: ""
-    }
-  };
+  // state = {
+  //   friend: {
+  //     name: "",
+  //     age: "",
+  //     email: ""
+  //   }
+  // };
 
-  handleInputChanges = e => {
-      this.setState({
-        friend: {
-          ...this.state.friend,
-          [e.target.name]: e.target.value
-        }
+  // handleInputChanges = e => {
+  //     this.setState({
+  //       friend: {
+  //         ...this.state.friend,
+  //         [e.target.name]: e.target.value
+  //       }
 
-      })
-  };
+  //     })
+  // };
 
-  addFriend = (e) => {
-    e.preventDefault();
-      this.props.postNewFriend(this.state.friend);
-  };
+  // addFriend = (e) => {
+  //   e.preventDefault();
+  //     this.props.postNewFriend(this.state.friend);
+  // };
 
-  updateFriend = e => {
-    e.preventDefault();
-    this.props.updateFriend(this.state.friend);
-  }
+  // updateFriend = e => {
+  //   e.preventDefault();
+  //   this.props.updateFriend(this.state.friend);
+  // }
 
   render() {
     return (
       <div className="friend-form">
         <FriendForm 
             postNewFriend={this.props.postNewFriend}
-            handleInputChanges={this.handleInputChanges}
-            addFriend={this.addFriend}
-            updateFriend={this.updateFriend}
-            friend={this.state.friend}
+            handleInputChanges={this.props.handleInputChanges}
+            addFriend={this.props.addFriend}
+            updateFriend={this.props.updateFriend}
+            friend={this.props.friend}
             friends={this.props.friends}
         />
       </div>
@@ -50,12 +50,14 @@ class FriendFormContainer extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
-  friends: state.friends,
-  isUpdating: state.isUpdating
-})
+// const mapStateToProps = state => ({
+//   friends: state.friends,
+//   isUpdating: state.isUpdating
+// })
 
-export default connect(
-  mapStateToProps,
-  { postNewFriend, updateFriend }
-)(FriendFormContainer);
+// export default connect(
+//   mapStateToProps,
+//   { postNewFriend, updateFriend }
+// )(FriendFormContainer);
+
+export default FriendFormContainer;
